d8d06374e223a4e3ea74f4b894ccb7321eef1912
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-10-25 00:47:04 -0600

    Removed a comment.

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,6 @@ const routes = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
 const profileController = require('../controllers/profile');
 
-// console.log('/GET USER PROFILE');
 routes.get('/', requiresAuth(), profileController.getProfile);
 
 module.exports = routes;
